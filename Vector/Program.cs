ee99355e485a2042c950f0f225639853755731b6
itazushuwa <demonize4532@gmail.com>
2023-03-24 21:15:31 +0700

    Добавьте файлы проекта.

---
+++ Vector/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Vector
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Vector vector = new Vector(2, 7);
+            Console.WriteLine(vector.ToString());
+            vector.X = 0;
+            vector.Y = -1;
+            Console.WriteLine(vector.ToString());
+            vector = new Vector(10, 50);
+            Console.WriteLine(vector.ToString());
+            Console.WriteLine(new Vector(0, 0).ToString());
+        }
+    }
+}
